d9205f8abef01f2f68e36103d78a4df96fe0cad9
Gonzalo Torres Arroyo <torres.argon25@triana.salesianos.edu>
2026-05-11 09:20:36 +0200

    Proyecto base

---
+++ GonzaloTorres_TutorMatch/src/main/java/com/salesianostriana/dam/gonzalotorres_tutormatch/GonzaloTorresTutorMatchApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.gonzalotorres_tutormatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GonzaloTorresTutorMatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GonzaloTorresTutorMatchApplication.class, args);
+	}
+
+}
